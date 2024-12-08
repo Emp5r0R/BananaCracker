d21a66b5e4f8ef5f02b7f6e55cddff8c2a304325
apocalypsecracker <rajasekaran572005@gmail.com>
2024-12-08 22:06:10 +0530

    Major Update: v3.3.3, For Details view ReleaseNotes.md

--- README.md
+++ README.md
@@ -3,8 +3,18 @@ This Project takes input of hash for first argument and File location as second 
 
 Modes:
  -v,--verbose
- -n,--normal
-Note: Modes are Mandatory option
+ -m,--multifast
+Example Usage For Multi Fast mode: 
+Type 1 --> ./sha256_cracker e2c587ac5155e215da971f07fb1aba71788ef2fa423a7efb251267419e146521 /usr/share/wordlists/rockyou.txt -m 
+Type 2 --> ./sha256_cracker ebf13de02b7406fd14c97ec3d136e6b36a27b53dd327602e7ace6912ccbccfa9 /usr/share/wordlists/rockyou.txt --multifast 
+Type 3 --> ./sha256_cracker path/to/the/hashes.txt /usr/share/wordlists/rockyou.txt -m
+Type 4 --> ./sha256_cracker path/to/the/hashes.txt /usr/share/wordlists/rockyou.txt
+Likewise for Verbose mode:
+Type 1 --> ./sha256_cracker e2c587ac5155e215da971f07fb1aba71788ef2fa423a7efb251267419e146521 /usr/share/wordlists/rockyou.txt -v 
+Type 2 --> ./sha256_cracker ebf13de02b7406fd14c97ec3d136e6b36a27b53dd327602e7ace6912ccbccfa9 /usr/share/wordlists/rockyou.txt --verbose 
+Type 3 --> ./sha256_cracker path/to/the/hashes.txt /usr/share/wordlists/rockyou.txt -v 
+
+**Note: Multi Fast mode is significantly faster than Verbose, thus choosing Multi Fast mode would be a wise choice. Also if no modes were specified then Multi Fast mode will be choosen automatically**
 ___________________________________________________________________________________________________________________________________________________________________________
 ### Usage for Linux Binary
 - Download the latest version Binary from here:- https://github.com/apocalypsecracker/SHA256_cracker/releases/latest
@@ -13,6 +23,27 @@ ________________________________________________________________________________
 chmod +x sha256_cracker
 ```
 - then, run :- `./sha256_cracker <hash> <Path_To_The_wordlist> <Mode>`
+
+-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+- **Placing into the executable path(Optional)**
+
+* I know always changing into the directory where sha256_cracker lives and executing it, is a pain in the ass. Don't worry I have a solution for you, Do the following steps carefully:
+- 'cd' into the directory where sha256_cracker is stored generally in Downloads.
+- Now type this in the terminal: 
+```
+sudo mv sha256_cracker /usr/local/bin
+
+```
+* That's all you've made it, Now you can execute sha256_cracker cracker anywhere from the Terminal Emulator like this:
+```
+sha256_cracker
+```
+___________________________________________________________________________________________________________________________________________________________________________
+### Usage for Windows executable
+- Download the latest version Binary from here:- https://github.com/apocalypsecracker/SHA256_cracker/releases/latest
+- Open Command Prompt or Powershell:
+- Now go to the path where sha256_cracker.exe located at or Open cmd from the path
+- then, run :- `./sha256_cracker.exe <hash> <Path_To_The_wordlist> <Mode>`
 ___________________________________________________________________________________________________________________________________________________________________________
 ### Usage for Source
 - Get into the directory of the project(eg: `cd sha256_cracker`)
@@ -24,8 +55,13 @@ ________________________________________________________________________________
 ___________________________________________________________________________________________________________________________________________________________________________
 ### Creator Answered Questions
 **Which should I choose, Binary or Source?**
-- In my opinion Choosing binary would be better because it is much easier to use and less likely to get in to issue. Building from source is different and much more complicated, There are also prerequesite for building from source like cargo need to be installed. However if can manage technical things in building and your confident that you can troubleshoot any issues and also if you want new updated version with new features and fixes (Note:might also have bugs too) then this should be your choice  
+- In my opinion Choosing binary would be better because it is much easier to use and less likely to get in to issue. **Binaries are much more faster.** Building from source is different and much more complicated, There are also prerequesite for building from source like cargo need to be installed. However if you can manage technical things in building and your confident that you can troubleshoot any issues and also if you want new updated version with new features and fixes (Note:might also have bugs too) then this should be your choice  
 
-**What are the versions of executable binaries, which tagged as Pre-release?**
+**What are ignored words?**
+While reading a wordlist and it's contents some words may contain unknown or complicated structure of words and symbols in a way where it cannot be converted to sha256 hash. Those words will be ignored and moved to next. 
+
+**What are the versions of executable binaries, which are tagged as Pre-release?**
 - Pre-release versions are generally not optimized binaries which can work fine but are slow and takes more disk space than Optimized binaries.
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,176 @@
-use colored::Colorize;
+use colored::Colorize; // For output color highlights
 use sha2::{Digest, Sha256}; // For SHA256 hashing
-use std::env;
+use std::env; // For taking Arguments
 use std::fs::File; // For file management
 use std::io::{BufRead, BufReader}; // For file reading
-use std::process::exit;
+use std::process::exit; // For closing the program
 
 fn main() {
-    // Arguments management
+    // Argument management
     let argue: Vec<String> = env::args().collect();
+    let version = env!("CARGO_PKG_VERSION");
+    println!(
+        " {} {} {}",
+        "
 
-    if argue.len() == 4 {
-        let req_hash = &argue[1];
-        let pass_file_loc = &argue[2];
-        let cust_option = &argue[3];
-        let mut current_state = 1;
-        let mut read_error_count = 0;
+ ┓   ┏┓┏━┏┓        ┓     
+┏┣┓┏┓┏┛┗┓┣┓ ┏┏┓┏┓┏ ┃┏ ┏┓ ┏┓
+┛┛┗┗┻┗━┗┛┗┛━┗┛ ┗┻┗ ┛┗ ┗  ┛ 
+"
+        .magenta(),
+        version.magenta(),
+        "https://github.com/apocalypsecracker/SHA256_cracker".magenta()
+    );
 
-        println!(
-            "{} Process started: Attempting to crack the hash: {}",
-            "[+]".blue(),
-            req_hash
-        );
-        println!(
-            "{} Specified wordlist path: {}",
-            "[+]".blue(),
-            pass_file_loc
-        );
+    // Determine mode argument
+    let mode_arg = if argue.len() == 4 {
+        &argue[3]
+    } else {
+        "--multifast"
+    };
 
-        // File management
-        let file_reader = match file_pass(pass_file_loc) {
-            Ok(reader) => reader,
-            Err(err) => {
-                eprintln!("{} Failed to open file: {}", "[X]".red(), err);
-                exit(1);
+    if argue.len() == 4 || argue.len() == 3 {
+        // Display mode selected
+        match (argue.len(), mode_arg) {
+            (3, _) => println!(
+                "{} No options selected, so Defaulting to {} mode",
+                "[!]".yellow(),
+                "Multi Fast".blue()
+            ),
+
+            (_, "--verbose") | (_, "-v") => {
+                println!("{} Performing in {} mode", "[+]".blue(), "Verbose".blue())
             }
+            (_, "--multifast") | (_, "-m") => {
+                println!("{} Performing in {} mode", "[+]".blue(), "Muti Fast".blue())
+            }
+            _ => println!(
+                "{} Invalid Option found, so Defaulting to {} mode",
+                "[!]".yellow(),
+                "Multi Fast".blue()
+            ),
+        };
+        let req_hash = argue[1].trim().to_string(); // Input hash or path to hash file
+        let pass_file_loc = &argue[2]; // Path to wordlist file
+
+        // Determine if input is a hash file or a single hash
+        let hashes_to_crack = if req_hash.contains(".txt") {
+            // Read hashes from file
+            let file_reader = file_pass(&req_hash).expect("Failed to read hash file");
+            file_reader
+                .lines()
+                .filter_map(|line| line.ok()) // Collect valid lines
+                .collect::<Vec<_>>() // Store all hashes
+        } else {
+            vec![req_hash] // Treat as a single hash
         };
 
-        // Main loop
-        for line in file_reader.lines() {
-            match line {
-                // Handling the errors
-                Ok(contents) => {
-                    let cont_pass = contents.trim().to_owned().into_bytes();
-                    let cont_pass_hash = format!("{:x}", Sha256::digest(&cont_pass));
-                    if &cont_pass_hash == req_hash {
-                        println!(
-                            "{} Password found [!] ----> {} <---- [!] Hash Cracked. Total attempts: {}. Found password's hash: {}",
-                            "[✔]".green(),
-                            std::str::from_utf8(&cont_pass).unwrap().green(),
-                            current_state,
-                            cont_pass_hash
-                        );
+        let mut total_hashes_cracked = 0; // Counter for successfully cracked hashes
+        let mut read_error_count = 0; // Counter for ignored lines due to errors
 
-                        exit(0);
-                    }
-                    current_state += 1;
-                    if (cust_option == "-v") || (cust_option == "--verbose") {
-                        println!(
-                            "{} Current Attempt: [{}], Current password: {} ----> {}",
-                            "[✖]".red(),
-                            current_state,
-                            std::str::from_utf8(&cont_pass).unwrap().red(),
-                            cont_pass_hash
-                        );
-                    }
+        println!(
+            "{} Specified wordlist path: {}\n",
+            "[+]".blue(),
+            pass_file_loc.blue()
+        );
+        // Loop over each hash to crack
+        for hash in hashes_to_crack {
+            if !hash.contains(".txt") && !hash.contains("/") {
+                println!(
+                    "{}. Attempting to crack hash: {}",
+                    total_hashes_cracked + 1,
+                    hash.blue()
+                );
+            } else {
+                println!(
+                    "{} Hash contains Invalid characters or unknown file location, check your inputs!",
+                    "[x]".red()
+                );
+                exit(0);
+            }
+
+            // Open the wordlist file
+            let file_reader = match file_pass(pass_file_loc) {
+                Ok(reader) => reader,
+                Err(err) => {
+                    eprintln!("{} Failed to open wordlist file: {}", "[X]".red(), err);
+                    continue; // If there's an error opening the wordlist, skip to the next hash
                 }
-                Err(_) => {
-                    read_error_count += 1;
+            };
+
+            let mut hash_cracked = false; // Flag to track if hash is cracked
+            let mut attempt_count = 0;
+
+            // Attempt to crack the hash using the wordlist
+            for line in file_reader.lines() {
+                match line {
+                    Ok(password) => {
+                        attempt_count += 1;
+                        let trimmed_password = password.trim().to_owned().into_bytes();
+                        let hashed_password = format!("{:x}", Sha256::digest(&trimmed_password));
+
+                        // Check if the password hash matches
+                        if hashed_password == hash {
+                            println!(
+                                "{} Password found! ----> {} <---- [Hash: {}] | Total Attempts: {}\n",
+                                "[✔]".green(),
+                                std::str::from_utf8(&trimmed_password).unwrap().green(),
+                                hash.green(),
+                                attempt_count - 1,
+                            );
+                            total_hashes_cracked += 1;
+                            hash_cracked = true;
+
+                            break; // If the hash is cracked, move to the next hash
+                        }
+
+                        // Verbose mode output
+                        if mode_arg == "-v" || mode_arg == "--verbose" {
+                            println!(
+                                "{} Attempt {} | Password: {} | Hash: {}",
+                                "[x]".red(),
+                                attempt_count,
+                                std::str::from_utf8(&trimmed_password).unwrap(),
+                                hashed_password
+                            );
+                        }
+                    }
+                    Err(_) => {
+                        read_error_count += 1; // Increase the error count if reading the line fails
+                    }
                 }
             }
+
+            // If the hash was not cracked, print a failure message
+            if !hash_cracked {
+                println!(
+                    "{} Failed to crack hash: {} | Total Failed attempts {}\n",
+                    "[✖]".red(),
+                    hash.red(),
+                    attempt_count
+                );
+            }
         }
+
+        // Final report on cracked hashes
         println!(
-            "{} Password hash not found! Total read errors: {}. Advised to try a different wordlist!",
-            "[X]".red(),
+            "\n{} Completed! Total Hashes Cracked: {}, Ignored Words: {}",
+            "[✔]".yellow(),
+            total_hashes_cracked,
             read_error_count
         );
     } else {
-        println!("{}", "Missing Options!".red());
+        println!("{}", "Invalid Options!".red());
         println!(
-            "{} {} {} {} {}",
-            "Usage --> For Raw Program:",
+            "Usage --> For Source : {} | --> For Binary: {} {}",
             "cargo run <hash> <Path_to_the_wordlist> <Mode>".blue(),
-            "| --> For Binary:",
             "./sha256_cracker <hash> <Path_to_the_wordlist> <Mode>".blue(),
-            "\nModes:\n -v,--verbose\n -n,--normal".blue()
+            "\nModes:\n -v,--verbose\n -m,--multifast".blue()
+        );
+        println!("{}","Example Usage: \nType 1 --> ./sha256_cracker e2c587ac5155e215da971f07fb1aba71788ef2fa423a7efb251267419e146521 /usr/share/wordlists/rockyou.txt -m \nType 2 --> ./sha256_cracker ebf13de02b7406fd14c97ec3d136e6b36a27b53dd327602e7ace6912ccbccfa9 /usr/share/wordlists/rockyou.txt --multifast \nType 3 --> ./sha256_cracker path/to/the/hashes.txt /usr/share/wordlists/rockyou.txt -m \nLikewise for Verbose mode use option -v or --verbose\n".blue());
+        println!(
+            "{} Tip: Multi Fast mode is significantly faster than Verbose mode",
+            "[!]".yellow()
         );
         exit(1);
     }
